b0d45dcb24aa5eeaf775ddd303e6e59ffb4ed775
Caroline Souza <loh_jg@hotmail.com>
2021-11-08 18:46:48 -0300

    pesquisa todos clientes

--- backend/src/main/java/com/projetoFinal/sistemapedidos/services/ClienteService.java
+++ backend/src/main/java/com/projetoFinal/sistemapedidos/services/ClienteService.java
@@ -1,11 +1,15 @@
 package com.projetoFinal.sistemapedidos.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.projetoFinal.sistemapedidos.dto.ClienteDTO;
 import com.projetoFinal.sistemapedidos.entities.Cliente;
+import com.projetoFinal.sistemapedidos.entities.Produto;
 import com.projetoFinal.sistemapedidos.repositories.ClienteRepository;
 
 @Service
@@ -22,9 +26,17 @@ public class ClienteService {
 		entity = repository.save(entity);
 		
 		return new ClienteDTO(entity);
+	}
+	
+	public List<ClienteDTO> findAll() {
+		List<Cliente> list= repository.findAll();
+		List<ClienteDTO>listDto = new ArrayList<>();
 		
-	
-	
+		for(Cliente cob : list) {
+			listDto.add(new ClienteDTO(cob));
+		}
+		
+		return listDto;
 	}
 
 }
