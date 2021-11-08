699ee861546178d1acb0832fbbfeebc81db7931c
Caroline Souza <loh_jg@hotmail.com>
2021-11-08 11:07:10 -0300

    first commit

---
+++ backend/src/test/java/com/projetoFinal/sistemapedidos/SistemapedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoFinal.sistemapedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemapedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
